16241fa6b8270d96732d3701e93e352c6da8b83b
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-12 19:58:01 -0300

    Update Program.cs

--- Olá Mundo/Olá Mundo/Program.cs
+++ Olá Mundo/Olá Mundo/Program.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Windows.Forms; //Usando a referência de interface gráfica Windows Forms.
 
 namespace Olá_Mundo
 {
