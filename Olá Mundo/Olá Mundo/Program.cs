e53becbd3a44ddb92ce910cb8483dd748f5bae55
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-12 18:48:36 -0300

    Update Program.cs

--- Olá Mundo/Olá Mundo/Program.cs
+++ Olá Mundo/Olá Mundo/Program.cs
@@ -6,8 +6,8 @@ namespace Olá_Mundo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Olá Mundo!");
-            Console.WriteLine("Bem-Vindo!");
+            Console.WriteLine("Olá Mundo!!");
+            Console.WriteLine("Bem-Vindo!!");
         }
     }
 }
