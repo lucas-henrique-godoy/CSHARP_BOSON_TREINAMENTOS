6ab5f4bb1f23fb7f530db130d023717bbac1a0e5
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-13 20:35:16 -0300

    atualizei minhas anotações sobre operadores condicionais.

--- Operadores Aritméticos/Operadores Aritméticos/Program.cs
+++ Operadores Aritméticos/Operadores Aritméticos/Program.cs
@@ -13,11 +13,11 @@ namespace Operadores_Aritméticos
             int num1 = 20;
             int num2 = 11;
 
-            Console.WriteLine("Soma: " + (num1 + num2));
-            Console.WriteLine("Subtração: " + (num1 - num2));
-            Console.WriteLine("Multiplicação: " + (num1 * num2));
-            Console.WriteLine("Divisão: " + (num1 / num2));
-            Console.WriteLine("Módulo: " + (num1 % num2));
+            Console.WriteLine("Soma: " + (num1 + num2).ToString());
+            Console.WriteLine("Subtração: " + (num1 - num2).ToString());
+            Console.WriteLine("Multiplicação: " + (num1 * num2).ToString());
+            Console.WriteLine("Divisão: " + (num1 / num2).ToString());
+            Console.WriteLine("Módulo: " + (num1 % num2).ToString());
 
             Console.ReadLine();
 
