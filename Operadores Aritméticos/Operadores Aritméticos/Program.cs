90403a7ab08ec7f4cf711f44272c67e2256b1a0a
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-13 20:18:35 -0300

    Criei o Operadores Aritméticos em C#.

---
+++ Operadores Aritméticos/Operadores Aritméticos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores_Aritméticos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 20;
+            int num2 = 11;
+
+            Console.WriteLine("Soma: " + (num1 + num2));
+            Console.WriteLine("Subtração: " + (num1 - num2));
+            Console.WriteLine("Multiplicação: " + (num1 * num2));
+            Console.WriteLine("Divisão: " + (num1 / num2));
+            Console.WriteLine("Módulo: " + (num1 % num2));
+
+            Console.ReadLine();
+
+        }
+    }
+}
