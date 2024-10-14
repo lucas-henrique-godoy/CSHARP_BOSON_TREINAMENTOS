778c02817604e3bb0aa9c53f73d1ad531dcf36e6
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-13 21:29:11 -0300

    .

--- Variáveis/Variáveis/Program.cs
+++ Variáveis/Variáveis/Program.cs
@@ -10,6 +10,8 @@ namespace Variáveis
     {
         static void Main(string[] args)
         {
+
+            /*
             int x = 90;
             bool a = false;
             int y;
@@ -17,7 +19,20 @@ namespace Variáveis
             Console.WriteLine("O número é: " + x.ToString());
             Console.WriteLine("E o valor lógico é: " + a.ToString());
             Console.WriteLine("O número é: " + y.ToString());
+            */
 
+            double a = 5.0;
+            double b = 0.0;
+
+            Console.WriteLine("Valor dividido por zero: " + (a / b));
+            Console.WriteLine("Zero dividido por um valor qualquer: " + (b / a));
+            Console.WriteLine("Zero dividido por zero: " + (b / b));
+            Console.WriteLine("Soma de NaN com um valor qualquer, como 15: " + (15 + (b / b)));
+            Console.WriteLine("Soma de Infinito com um valor qualquer, como 15: " + (15 + (a / b)));
+            Console.WriteLine("Zero vezes Infinito: " + (0 * (a / b)));
+            Console.WriteLine("Zero vezes NaN: " + (0 * (b / b)));
+
+           
             Console.ReadLine();
         }
     }
