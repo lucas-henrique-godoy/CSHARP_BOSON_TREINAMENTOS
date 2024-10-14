a578d051a92443f87b6266a64e68b2b9859e598f
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-14 10:40:52 -0300

    rsolvi o problema do terminal de não mostrar o símbolo de ∞

---
+++ Operadores Aritméticos/Operadores Aritméticos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Operadores_Aritméticos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Define a codificação de saída para UTF-8 e possibilita mostrar o símbolo de infinity:  ∞
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            double a = 5.0;
+            double b = 0.0;
+
+            Console.WriteLine("Valor dividido por zero: " + (a / b)); // Infinito
+            Console.WriteLine("Zero dividido por um valor qualquer: " + (b / a)); // 0
+            Console.WriteLine("Zero dividido por zero: " + (b / b)); // NaN
+            Console.WriteLine("Soma de NaN com um valor qualquer, como 15: " + (15 + (b / b))); // NaN
+            Console.WriteLine("Soma de Infinito com um valor qualquer, como 15: " + (15 + (a / b))); // Infinito
+            Console.WriteLine("Zero vezes Infinito: " + (0 * (a / b))); // NaN
+            Console.WriteLine("Zero vezes NaN: " + (0 * (b / b))); // NaN
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Teste/Teste/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 5.0;
+            double b = 0.0;
+
+            Console.WriteLine("Valor dividido por zero: " + (a / b));
+            Console.WriteLine("Zero dividido por um valor qualquer: " + (b / a));
+            Console.WriteLine("Zero dividido por zero: " + (b / b));
+            Console.WriteLine("Soma de NaN com um valor qualquer, como 15: " + (15 + (b / b)));
+            Console.WriteLine("Soma de Infinito com um valor qualquer, como 15: " + (15 + (a / b)));
+            Console.WriteLine("Zero vezes Infinito: " + (0 * (a / b)));
+            Console.WriteLine("Zero vezes NaN: " + (0 * (b / b)));
+
+            Console.ReadLine();
+
+        }
+    }
+}
