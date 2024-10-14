a48309556fae9cc88a27fc206201ace845c4ce6a
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-14 11:33:19 -0300

    .

---
+++ Operadores/Operadores/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Operadores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Operadores de Atribuição
+            // =, +=, -=, *=, /*
+        }
+    }
+}
