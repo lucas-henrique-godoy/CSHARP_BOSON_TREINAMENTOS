16c6e4c514e112eecc08dbd441e4b5619d49bd51
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-15 12:09:08 -0300

    criei o Condicional Simples e Composto

---
+++ Condicional_Simples_e_Composto/Condicional_Simples_e_Composto/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Condicional_Simples_e_Composto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Condional Simples            
+            int num1;
+            int num2;            
+            Console.WriteLine("Digite um número: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro número: ");
+            num2 = int.Parse(Console.ReadLine());
+            if(num1 == num2)
+            {
+                Console.WriteLine("Os números são iguais");
+            }
+            */
+
+
+
+            // Condicional Composto
+            int num1;
+            int num2;
+            Console.WriteLine("Digite um número: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro número: ");
+            num2 = int.Parse(Console.ReadLine());
+            if (num1 == num2)
+            {
+                Console.WriteLine("Os números são iguais");
+            }
+            else
+            {
+                Console.WriteLine("Os números são diferentes");
+            }
+        }
+    }
+}
