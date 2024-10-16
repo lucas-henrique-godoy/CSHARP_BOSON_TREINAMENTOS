354b0dc6576081dd071ba7352e2d4e50d5118c23
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-16 12:08:18 -0300

    create Laço For

---
+++ Laço For/Laço For/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Laço_For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int contador = 1; contador <= 50; contador++)
+            {
+                Console.WriteLine(contador);
+            }
+            
+        }
+    }
+}
