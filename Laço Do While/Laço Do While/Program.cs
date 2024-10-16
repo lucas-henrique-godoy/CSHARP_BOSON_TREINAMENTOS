8b66b06d968d8ee7416b2ea60a7b9f284b89c02c
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-16 12:52:07 -0300

    Update Program.cs

--- Laço Do While/Laço Do While/Program.cs
+++ Laço Do While/Laço Do While/Program.cs
@@ -6,6 +6,7 @@ namespace Laço_Do_While
     {
         static void Main(string[] args)
         {
+            /*
             int contador = 1;
             do
             {
@@ -13,6 +14,21 @@ namespace Laço_Do_While
                 contador++;
             }
             while (contador <= 50);
+            */
+
+
+            // PSEUDO-LOOP INFINITO: O código continua pedindo ao usuário para digitar números até que ele digite o número 10. Quando isso acontece, o loop se encerra e uma mensagem de fim é exibida.
+            
+            int contador;
+            do
+            {
+                Console.WriteLine("Digite um número: ");
+                contador = int.Parse(Console.ReadLine());
+                Console.WriteLine("O número digitado foi: {0}", contador);
+            }
+            while (contador != 10);
+
+            Console.WriteLine("Fim do loop!");
         }
     }
 }
