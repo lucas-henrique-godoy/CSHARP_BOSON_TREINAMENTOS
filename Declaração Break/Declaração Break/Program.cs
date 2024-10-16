306aaa2c9934a8c700e0b5c571bc5d7a45f58459
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-16 15:20:10 -0300

    update Declaração Break

---
+++ Declaração Break/Declaração Break/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Declaração_Break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            while (true)
+            {
+                Console.WriteLine("Digite um número; número 15 encerra o loop");
+                numero = int.Parse(Console.ReadLine());
+                if (numero == 15)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Número digitado: {0}, ", numero);
+                }                
+            }
+            Console.WriteLine("Fim do loop!");
+        }
+    }
+}
