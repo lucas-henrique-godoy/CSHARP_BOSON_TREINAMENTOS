1267777858c5b18b7d69cc37195d3b51c2e11240
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-16 10:09:39 -0300

    create Loop While

---
+++ Loop While/Loop While/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Loop_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            while (contador <= 10)
+            {
+                Console.WriteLine(contador);
+                contador++;
+            }
+            
+        }
+    }
+}
