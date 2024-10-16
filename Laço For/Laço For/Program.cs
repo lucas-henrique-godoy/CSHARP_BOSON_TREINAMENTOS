e6cccd791ded70d58d38ab62bc315df3fb40a431
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-16 12:13:49 -0300

    Update Program.cs

--- Laço For/Laço For/Program.cs
+++ Laço For/Laço For/Program.cs
@@ -2,6 +2,8 @@
 
 namespace Laço_For
 {
+    /*Neste caso, a variável contador terá como escopo o próprio lãço for, ou seja só é válida
+    dentro do loop, ao encrrarmos o loop, a variável contador é desalocada.*/
     internal class Program
     {
         static void Main(string[] args)
