6b5fe8cac1978f0ea2488cb655f05208494de63c
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-16 12:35:06 -0300

    update ANOTACOES

---
+++ Laço Do While/Laço Do While/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Laço_Do_While
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+            do
+            {
+                Console.WriteLine(contador);
+                contador++;
+            }
+            while (contador <= 50);
+        }
+    }
+}
