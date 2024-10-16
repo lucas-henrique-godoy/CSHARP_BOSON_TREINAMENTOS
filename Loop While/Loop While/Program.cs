d8cdce6fb101188a1dd85b5d6dea69a892e5a007
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-16 11:32:55 -0300

    Update Program.cs

--- Loop While/Loop While/Program.cs
+++ Loop While/Loop While/Program.cs
@@ -6,12 +6,29 @@ namespace Loop_While
     {
         static void Main(string[] args)
         {
+            /* CONTANDO DE 1 A 50 E EXIBINDOS OS NÚMEROS DE 1 A 50
             int contador = 1;
             while (contador <= 50)
             {
                 Console.WriteLine(contador);
                 contador++;
-            }            
+            }
+            */
+
+
+            // LOOP PSEUDO-INFINITO- Criando um loop que continua solicitando ao usuário que digite uma letra até que ele digite a letra "a"
+            
+            string letra;
+            Console.WriteLine("Digite uma letra; a para encerrar ");
+            letra = Console.ReadLine();
+
+            while (letra != "a")
+            {
+                Console.WriteLine("Digite uma letra: [a para encerrar!] ");
+                letra = Console.ReadLine();
+                Console.WriteLine(letra);                
+            }
+            Console.WriteLine("Fim do loop!");
         }
     }
 }
