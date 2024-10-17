7f098238b8db8e835be20f8853fa09f9444c29dc
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-17 00:11:20 -0300

    update

---
+++ Arrays 01/Arrays 01/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Arrays_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros_loteria; // Declaração doa array
+            numeros_loteria = new int[6]; //Instanciação do array
+            
+        }
+    }
+}
