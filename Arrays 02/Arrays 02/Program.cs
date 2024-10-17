419526a99923d7be14dde8a793c1deef8a74529b
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-17 15:57:52 -0300

    Update Program.cs

--- Arrays 02/Arrays 02/Program.cs
+++ Arrays 02/Arrays 02/Program.cs
@@ -8,7 +8,7 @@ namespace Arrays_02
         {
             /* EXEMPLO 1
             int[] meuarray = { 2, 4, 6, 3, 5, 9 };
-            for (int i = 0; i < meuarray.Length; i++) // Length - Obtem total de elementos do array
+            for (int i = 0; i < meuarray.Length; i++) // Length - Obtém o total de elementos do array
             {
                 Console.WriteLine(meuarray[i]);
             }
@@ -20,20 +20,18 @@ namespace Arrays_02
             // Preenchendo o array
             for (int i = 0; i < meuarray.Length; i++)
             {
-                // Pedindo ao usuário que informe os valores para serem colocados em cada posição do array
+                // Pedindo ao usuário que informe os valores para serem armazenados em cada posição do array
                 Console.WriteLine("Digite um número: ");
                 meuarray[i] = Convert.ToInt32(Console.ReadLine());
             }
-            
+
             Console.WriteLine();
 
             // Mostrando os valores do array
             for (int i = 0; i < meuarray.Length; i++)
             {
-                Console.WriteLine("Posição {0}: {1}", i, meuarray[i]);  
+                Console.WriteLine("Posição {0}: {1}", i, meuarray[i]);
             }
-
-
         }
     }
 }
