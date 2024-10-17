3c16a9eb1977822f4fcb913cf9445bfd9b76ff77
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-17 15:12:57 -0300

    update Arrays_02

---
+++ Arrays 02/Arrays 02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Arrays_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] meuarray = { 2, 4, 6, 3, 5, 9 };
+            for (int i = 0; i < meuarray.Length; i++) // Length - Obtem total de elementos do array
+            {
+                Console.WriteLine(meuarray[i]);
+            }
+
+        }
+    }
+}
