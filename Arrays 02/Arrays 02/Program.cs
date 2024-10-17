ef791d87d7ea68b797cb1b92d2ae4bac76f6d7bc
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-17 15:56:54 -0300

    Update Program.cs

--- Arrays 02/Arrays 02/Program.cs
+++ Arrays 02/Arrays 02/Program.cs
@@ -6,11 +6,33 @@ namespace Arrays_02
     {
         static void Main(string[] args)
         {
+            /* EXEMPLO 1
             int[] meuarray = { 2, 4, 6, 3, 5, 9 };
             for (int i = 0; i < meuarray.Length; i++) // Length - Obtem total de elementos do array
             {
                 Console.WriteLine(meuarray[i]);
             }
+            */
+
+            // Declarando um array de 5 posições
+            int[] meuarray = new int[5];
+
+            // Preenchendo o array
+            for (int i = 0; i < meuarray.Length; i++)
+            {
+                // Pedindo ao usuário que informe os valores para serem colocados em cada posição do array
+                Console.WriteLine("Digite um número: ");
+                meuarray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            
+            Console.WriteLine();
+
+            // Mostrando os valores do array
+            for (int i = 0; i < meuarray.Length; i++)
+            {
+                Console.WriteLine("Posição {0}: {1}", i, meuarray[i]);  
+            }
+
 
         }
     }
