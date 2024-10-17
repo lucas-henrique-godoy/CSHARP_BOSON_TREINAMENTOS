20ff125dd90eba9078ab9103eaf0f4904adb99eb
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-17 11:27:00 -0300

    Update Program.cs

--- Arrays 01/Arrays 01/Program.cs
+++ Arrays 01/Arrays 01/Program.cs
@@ -6,8 +6,11 @@ namespace Arrays_01
     {
         static void Main(string[] args)
         {
-            int[] numeros_loteria; // Declaração doa array
-            numeros_loteria = new int[6]; //Instanciação do array
+            int[] numeros_loteria = new int[6] { 2,23,34,12,29,44 }; // Declaração e instanciação do array.           
+            int valor;
+            valor = 40;
+            numeros_loteria[4] = valor;
+            Console.WriteLine("O elemento é {0}", numeros_loteria[4].ToString());
             
         }
     }
