c60beb1770ed8f8e39fe093886a79324191718e6
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-18 12:13:44 -0300

    create Copiando E Clonado Arrays

---
+++ Copiando E Clonando Arrays/Copiando E Clonando Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Copiando_E_Clonando_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 4,8,12,16,20 };
+            int[] copiaNumeros = new int[numeros.Length];
+
+            // copiando os elementos com o laço for:
+
+            for (int i = 0; i <)
+            {
+                ;
+            }
+            
+        }
+    }
+}
