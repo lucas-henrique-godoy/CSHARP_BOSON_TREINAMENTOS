6a1ecb1acfd6cfb4248e797558ec3b19938d89be
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-18 15:32:46 -0300

    create Clone Array

---
+++ Clone Array/Clone Array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Clone_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = { 4, 7, 13, 19, 15 };
+
+            // Clonando uma array:
+            int[] copia = (int[])numeros.Clone(); // O método Clone retorna um object porque é definido na classe base Array, e para atribuí-lo a um array de int, é necessário fazer um casting(conversão) explícito para int[].
+
+            for (int i = 0; i < copia.Length; i++)
+            {
+                Console.WriteLine(copia[i]);
+            }
+        }
+    }
+}
