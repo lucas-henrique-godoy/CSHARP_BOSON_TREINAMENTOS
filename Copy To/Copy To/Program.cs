3ba56cf4d193c1a403b9425a46976a41ba399ece
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-18 14:40:59 -0300

    update ANOTACOES

---
+++ Copy To/Copy To/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Copy_To
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[] { 3,6,9,12,15 };
+            int[] copiaNumeros = new int[numeros.Length];
+
+            // Copiando os elementos com o método CpyTo
+            numeros.CopyTo()
+        }
+    }
+}
