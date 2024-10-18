7b5ec8e8a9cc828584b21b0333211b6851110822
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-17 23:20:30 -0300

    create Foreach

---
+++ Foreach/Foreach/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] meuarray = { 2,4,6,8,10};
+            foreach (int elemento in meuarray)
+            {
+                Console.WriteLine(elemento);
+            }
+
+           
+        }
+    }
+}
