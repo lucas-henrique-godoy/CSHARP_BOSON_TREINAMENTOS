d423be99354b7881004ce99a251316fc88f2eebf
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-18 12:35:55 -0300

    upgrade program

--- Copiando E Clonando Arrays/Copiando E Clonando Arrays/Program.cs
+++ Copiando E Clonando Arrays/Copiando E Clonando Arrays/Program.cs
@@ -9,13 +9,17 @@ namespace Copiando_E_Clonando_Arrays
             int[] numeros = { 4,8,12,16,20 };
             int[] copiaNumeros = new int[numeros.Length];
 
-            // copiando os elementos com o laço for:
 
-            for (int i = 0; i <)
+            // copiando os elementos com o laço for:
+            for (int i = 0; i < numeros.Length; i++)
             {
-                ;
+                copiaNumeros[i] = numeros[i];
             }
-            
+
+            for (int i = 0; i < copiaNumeros.Length; i++)
+            {
+                Console.WriteLine(copiaNumeros[i]);
+            }
         }
     }
 }
