775bd377b43f66bf821b0e02680d4b2e9dce31be
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-19 23:56:43 -0300

    Update Program.cs

--- Métodos/Métodos/Program.cs
+++ Métodos/Métodos/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Métodos
 {
@@ -6,7 +7,23 @@ namespace Métodos
     {
         static void Main(string[] args) // Static - não preciso criar uma instânciada classe para usar esse método
         {
-            Console.WriteLine("Hello World!");
+            int soma;
+            int valor1;
+            int valor2;
+
+            Console.WriteLine("Digite um número: ");
+            valor1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro número: ");
+            valor2 = int.Parse(Console.ReadLine());
+            soma = somaNumeros(valor1, valor2);
+            Console.WriteLine("A soma é: {0}", soma.ToString());
+        }
+        // Método para somar dois números:
+        static int somaNumeros(int num1,int num2)
+        {
+            int resultado = num1 + num2;
+            return resultado;
+           
         }
     }
 }
