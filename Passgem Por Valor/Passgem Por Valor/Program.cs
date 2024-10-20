8587c7baaf349065093ae8969fdac329ffc7156a
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-20 00:21:54 -0300

    upgrade Program

---
+++ Passgem Por Valor/Passgem Por Valor/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Passgem_Por_Valor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Digite um número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            //chamando método
+            quadrado(numero);
+            //Mostrando que a variável original não foi alterada:
+            Console.WriteLine("O número digitado foi {0}", numero);
+        }
+
+        public static void quadrado(int num1)
+        {
+            int quad = num1 * num1;
+            Console.WriteLine(quad.ToString());
+        }
+    }
+}
