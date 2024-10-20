780dc75657bfa081a3e189aff0767c4e912cac3f
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-20 00:47:07 -0300

    update Program

---
+++ Passagem Por Referencia/Passagem Por Referencia/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Passagem_Por_Referencia
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine("Digite um número: ");
+            numero = int.Parse(Console.ReadLine());
+            incrementa(ref numero);
+            Console.WriteLine("O valor incrementado é: {0}", numero);
+        }
+
+        static void incrementa(ref int valor)
+        {
+            valor++;
+        }
+       
+
+    }
+}
