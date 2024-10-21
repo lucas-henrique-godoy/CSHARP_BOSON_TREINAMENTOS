f1a2d3ed656499dfd7153d7965d34195c5ca8088
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-21 16:51:29 -0300

    upgrade Program

---
+++ Modificadores de Acesso/Modificadores de Acesso/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modificadores_de_Acesso
+{
+    internal class Animal
+    {
+        private int idade = 10;
+        private double peso = 50.4;
+
+        public int obterIdade()
+        {
+            return idade;
+        }
+
+        public double obterPeso
+        {
+            get { return peso; }
+        }
+    }
+}
---
+++ Modificadores de Acesso/Modificadores de Acesso/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Modificadores_de_Acesso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            double peso;
+
+            Animal bicho = new Animal();
+            idade = bicho.obterIdade();
+            peso = bicho.obterPeso;           
+
+            Console.WriteLine("A idade é: {0}", idade);
+            Console.WriteLine("O peso é {0:F}", peso);
+        }
+    }
+}
