3e4361802247141b18714a1eb87a1d7294ad4b7a
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-20 22:24:46 -0300

    Update Program.cs

--- SobrecargaMétodos/SobrecargaMétodos/Program.cs
+++ SobrecargaMétodos/SobrecargaMétodos/Program.cs
@@ -6,13 +6,17 @@ namespace SobrecargaMétodos
     {
         static void Main(string[] args)
         {
+            // Usando o mesmo método potencia com 1 parâmetro.
             double resultado;
             int num1, num2;
             Console.WriteLine("Digite um nº para calcular seu  quadrado: ");
             num1 = int.Parse(Console.ReadLine());
             resultado = potencia(num1);
             Console.WriteLine("O qadrado de {0} é {1}", num1, resultado);
-            Console.WriteLine();
+
+            Console.WriteLine(); // Pula 1 inha
+
+            // Usando o mesmo método potencia com 2 parâmetros.
             Console.WriteLine("Digite dois nºs para calcular x ^ y: ");
             num1 = int.Parse(Console.ReadLine());
             num2 = int.Parse(Console.ReadLine());
