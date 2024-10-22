07c4ace7fff391cbddd4f7bbe93d859bb31e734c
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-22 16:22:13 -0300

    upgrade ANOTACOES

--- Modificador Static/Modificador Static/Program.cs
+++ Modificador Static/Modificador Static/Program.cs
@@ -6,17 +6,7 @@ namespace Modificador_Static
     {
         static void Main(string[] args)
         {
-            int idade;
-            double peso;
-
-            Animal bicho = new Animal();
-            idade = bicho.ObterIdade();
-            peso = bicho.ObterPeso;
-
-            Console.WriteLine("A idade é {0}", idade);
-            Console.WriteLine("O peso é {0:F}", peso);
-
-            Animal.Mensagem();
-        }
+            Animal.Mensagem(); // Executa o método estático Menssagem, não precisando instanciar a classe.
+        }                      // É só usar a classe.método.
     }
 }
