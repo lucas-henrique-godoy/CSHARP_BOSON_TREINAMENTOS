02b50fb81f90f7b148760a583f4623c0eeda7b7b
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-22 13:44:34 -0300

    create Modificador Static

---
+++ Modificador Static/Modificador Static/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Versioning;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modificador_Static
+{
+    internal class Animal
+    {
+        private int idade = 18;
+        public double peso = 50.4;
+
+        public int ObterIdade()
+        {
+            return idade;
+        }
+
+        public double ObterPeso
+        {
+            get { return peso;}
+        }
+    }
+}
---
+++ Modificador Static/Modificador Static/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Modificador_Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            double peso;
+
+            Animal bicho = new Animal();
+            idade = bicho.ObterIdade();
+            peso = bicho.ObterPeso;
+        }
+    }
+}
