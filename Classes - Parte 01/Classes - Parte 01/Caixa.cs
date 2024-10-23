8d1ec99dd1791b57e8df0042b23d827628960523
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-23 16:01:44 -0300

    create Classes - Parte 01

---
+++ Classes - Parte 01/Classes - Parte 01/Caixa.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes___Parte_01
+{
+    internal class Caixa
+    {
+        private double lado;
+
+        public Caixa() /// Método construtor padrão (inicializa lado com 10)
+        {
+            lado = 10;
+        }
+
+        public Caixa(double qualquerLado) // Método construtor sobrecarregado (inicializa lado com o valor fornecido pelo usuário)
+        {
+            lado = qualquerLado;
+        }
+
+        public double Volume() // Calcula o volume da caixa (lado^3)
+        {
+            return lado * lado * lado;
+        }
+    }
+}
---
+++ Classes - Parte 01/Classes - Parte 01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Classes___Parte_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Criação de uma instância da classe Caixa usando o construtor padrão
+            // Este construtor inicializa o lado com um valor fixo de 10
+            Caixa cx = new Caixa();
+
+            // Criação de uma instância da classe Caixa usando o construtor sobrecarregado
+            // Este construtor permite ao usuário especificar um valor para o lado (neste caso, 8)
+            Caixa cx2 = new Caixa(8);
+
+            double volumeCaixa;
+            double volumeCaixa2;
+
+            // Calcula o volume da primeira caixa usando o método Volume
+            volumeCaixa = cx.Volume();
+
+            // Calcula o volume da segunda caixa usando o método Volume
+            volumeCaixa2 = cx2.Volume();
+
+            // Exibe o volume da primeira caixa
+            Console.WriteLine(volumeCaixa.ToString());
+
+            // Exibe o volume da segunda caixa
+            Console.WriteLine(volumeCaixa2.ToString());            
+        }
+
+    }
+}
