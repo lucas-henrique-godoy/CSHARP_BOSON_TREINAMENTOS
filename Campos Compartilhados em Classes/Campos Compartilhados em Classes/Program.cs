ef69b212947122bdf806c2950b76ff0d46e039bb
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-24 10:46:02 -0300

    create Campos Compartilhados em Classes

---
+++ Campos Compartilhados em Classes/Campos Compartilhados em Classes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Campos_Compartilhados_em_Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
