ec4107c6411c373bc86891fd5ff8d4741691a227
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-27 19:53:07 -0300

    criei um novo projeto de Windows Forms App chamado Botões, que mostra como funcionam os botões.

--- Botões/Botões/Form1.Designer.cs
+++ Botões/Botões/Form1.Designer.cs
@@ -1,14 +1,14 @@
-﻿namespace GUI_01
+﻿namespace Botões
 {
-    partial class Janela2
+    partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        ///  Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        ///  Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
@@ -23,34 +23,37 @@
         #region Windows Form Designer generated code
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            btnCliqueMe = new System.Windows.Forms.Button();
             SuspendLayout();
             // 
-            // monthCalendar1
+            // btnCliqueMe
             // 
-            monthCalendar1.Location = new System.Drawing.Point(29, 36);
-            monthCalendar1.Name = "monthCalendar1";
-            monthCalendar1.TabIndex = 0;
+            btnCliqueMe.Location = new System.Drawing.Point(227, 75);
+            btnCliqueMe.Name = "btnCliqueMe";
+            btnCliqueMe.Size = new System.Drawing.Size(94, 52);
+            btnCliqueMe.TabIndex = 0;
+            btnCliqueMe.Text = "Clique Aqui";
+            btnCliqueMe.UseVisualStyleBackColor = true;
+            btnCliqueMe.Click += btnCliqueMe_Click;
             // 
-            // Janela2
+            // Form1
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            BackColor = System.Drawing.Color.Yellow;
-            ClientSize = new System.Drawing.Size(284, 261);
-            Controls.Add(monthCalendar1);
-            Name = "Janela2";
-            Text = "Form2";
+            ClientSize = new System.Drawing.Size(541, 233);
+            Controls.Add(btnCliqueMe);
+            Name = "Form1";
+            Text = "Form1";
             ResumeLayout(false);
         }
 
         #endregion
 
-        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Button btnCliqueMe;
     }
 }
