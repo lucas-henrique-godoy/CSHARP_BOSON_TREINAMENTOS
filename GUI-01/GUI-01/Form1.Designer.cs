1c6f139171cbffd5338c9077d1a0a3327cc7f19e
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-26 23:46:43 -0300

    Update Form1.Designer.cs

--- GUI-01/GUI-01/Form1.Designer.cs
+++ GUI-01/GUI-01/Form1.Designer.cs
@@ -30,9 +30,6 @@
         {
             label1 = new System.Windows.Forms.Label();
             label2 = new System.Windows.Forms.Label();
-            textBox1 = new System.Windows.Forms.TextBox();
-            textBox2 = new System.Windows.Forms.TextBox();
-            button1 = new System.Windows.Forms.Button();
             SuspendLayout();
             // 
             // label1
@@ -54,37 +51,11 @@
             label2.TabIndex = 1;
             label2.Text = "label2";
             // 
-            // textBox1
-            // 
-            textBox1.Location = new System.Drawing.Point(68, 29);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new System.Drawing.Size(100, 23);
-            textBox1.TabIndex = 2;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new System.Drawing.Point(68, 57);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new System.Drawing.Size(100, 23);
-            textBox2.TabIndex = 3;
-            // 
-            // button1
-            // 
-            button1.Location = new System.Drawing.Point(68, 98);
-            button1.Name = "button1";
-            button1.Size = new System.Drawing.Size(75, 23);
-            button1.TabIndex = 4;
-            button1.Text = "button1";
-            button1.UseVisualStyleBackColor = true;
-            // 
             // frmInicial
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             ClientSize = new System.Drawing.Size(488, 330);
-            Controls.Add(button1);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
             Controls.Add(label2);
             Controls.Add(label1);
             Name = "frmInicial";
@@ -97,8 +68,5 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
     }
 }
