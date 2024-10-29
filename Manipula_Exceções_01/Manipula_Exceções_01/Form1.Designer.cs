408234f0d276d3154275bae8de083ad13d3bc28a
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-29 15:27:47 -0300

    criei um mini projeto de um multiplicador de números inteiros no Windows Forms App, para estudar tratamento de exceções.

---
+++ Manipula_Exceções_01/Manipula_Exceções_01/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Manipula_Exceções_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtEntrada1 = new System.Windows.Forms.TextBox();
+            txtEntrada2 = new System.Windows.Forms.TextBox();
+            btnMultiplicar = new System.Windows.Forms.Button();
+            txtSaida = new System.Windows.Forms.TextBox();
+            groupBox1 = new System.Windows.Forms.GroupBox();
+            label1 = new System.Windows.Forms.Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // txtEntrada1
+            // 
+            txtEntrada1.Location = new System.Drawing.Point(134, 78);
+            txtEntrada1.Multiline = true;
+            txtEntrada1.Name = "txtEntrada1";
+            txtEntrada1.Size = new System.Drawing.Size(147, 33);
+            txtEntrada1.TabIndex = 0;
+            txtEntrada1.TextChanged += txtEntrada1_TextChanged;
+            // 
+            // txtEntrada2
+            // 
+            txtEntrada2.Location = new System.Drawing.Point(134, 137);
+            txtEntrada2.Multiline = true;
+            txtEntrada2.Name = "txtEntrada2";
+            txtEntrada2.Size = new System.Drawing.Size(147, 33);
+            txtEntrada2.TabIndex = 1;
+            // 
+            // btnMultiplicar
+            // 
+            btnMultiplicar.BackColor = System.Drawing.Color.Black;
+            btnMultiplicar.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            btnMultiplicar.ForeColor = System.Drawing.Color.White;
+            btnMultiplicar.Location = new System.Drawing.Point(151, 199);
+            btnMultiplicar.Name = "btnMultiplicar";
+            btnMultiplicar.Size = new System.Drawing.Size(105, 50);
+            btnMultiplicar.TabIndex = 2;
+            btnMultiplicar.Text = "Multiplicar";
+            btnMultiplicar.UseVisualStyleBackColor = false;
+            btnMultiplicar.Click += btnMultiplicar_Click;
+            // 
+            // txtSaida
+            // 
+            txtSaida.Location = new System.Drawing.Point(134, 281);
+            txtSaida.Multiline = true;
+            txtSaida.Name = "txtSaida";
+            txtSaida.Size = new System.Drawing.Size(147, 33);
+            txtSaida.TabIndex = 3;
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = System.Drawing.Color.FromArgb(224, 224, 224);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(txtEntrada1);
+            groupBox1.Controls.Add(txtSaida);
+            groupBox1.Controls.Add(txtEntrada2);
+            groupBox1.Controls.Add(btnMultiplicar);
+            groupBox1.Location = new System.Drawing.Point(158, 62);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new System.Drawing.Size(407, 343);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = System.Drawing.SystemColors.Desktop;
+            label1.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            label1.Location = new System.Drawing.Point(125, 19);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(167, 37);
+            label1.TabIndex = 4;
+            label1.Text = "Multiplicador";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            ClientSize = new System.Drawing.Size(707, 486);
+            Controls.Add(groupBox1);
+            Name = "Form1";
+            Text = "Form1";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEntrada1;
+        private System.Windows.Forms.TextBox txtEntrada2;
+        private System.Windows.Forms.Button btnMultiplicar;
+        private System.Windows.Forms.TextBox txtSaida;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Manipula_Exceções_01/Manipula_Exceções_01/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Manipula_Exceções_01
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int num1 = int.Parse(txtEntrada1.Text);
+                int num2 = int.Parse(txtEntrada2.Text);
+
+                int multiplicacao = num1 * num2;
+
+                txtSaida.Text = multiplicacao.ToString();
+            }
+            catch (FormatException ex)
+            {
+                MessageBox.Show(ex.Message, "Bloco catch");
+            }
+            finally
+            {
+                MessageBox.Show("Este bloco sempre será executado, independente de erros", "Bloco finally");
+            }
+        }
+
+        private void txtEntrada1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
