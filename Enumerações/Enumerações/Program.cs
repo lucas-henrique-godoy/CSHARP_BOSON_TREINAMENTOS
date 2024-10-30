b1d894272cf0abae1214057c308a1065f980dcd6
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-30 12:00:50 -0300

    Update Program.cs

--- Enumerações/Enumerações/Program.cs
+++ Enumerações/Enumerações/Program.cs
@@ -4,6 +4,7 @@ namespace Enumerações
 {
     internal class Program
     {
+        //enum PontoCardeal { Norte = 10, Sul, Leste, Oeste }; // Definindo uma enumeração chamada PontoCardeal com valores Norte, Sul, Leste e Oeste, onde Norte começa em 10 e os demais valores são incrementais.
         enum PontoCardeal { Norte, Sul, Leste, Oeste };
         static void Main(string[] args)
         {
@@ -13,6 +14,7 @@ namespace Enumerações
             // Mostrando o índice da posição do valor Norte dentro da enumeração PontoCardeal
             PontoCardeal direcao1 = PontoCardeal.Norte;
             Console.WriteLine("Ponto cardeal: " + (int)direcao1);
+            
         }
     }
 }
