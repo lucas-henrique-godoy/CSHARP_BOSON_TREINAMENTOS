6d3af2da26f5ad067fdc9829601e0d4c2f14bb7e
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-30 12:29:02 -0300

    Update Program.cs

--- Enumerações/Enumerações/Program.cs
+++ Enumerações/Enumerações/Program.cs
@@ -4,7 +4,7 @@ namespace Enumerações
 {
     internal class Program
     {
-        //enum PontoCardeal { Norte = 10, Sul, Leste, Este = Leste, Oeste }; // Definindo uma enumeração chamada PontoCardeal com valores Norte (10), Sul, Leste, Este (igual a Leste, ou seja tano faz chamar Este ou Leste) e Oeste, onde os valores são incrementais a partir de 10.
+        //OUTRO EXEMPLO: enum PontoCardeal { Norte = 10, Sul, Leste, Este = Leste, Oeste }; // Definindo uma enumeração chamada PontoCardeal com valores Norte (10), Sul, Leste, Este (igual a Leste, ou seja tano faz chamar Este ou Leste) e Oeste, onde os valores são incrementais a partir de 10.
         enum PontoCardeal { Norte, Sul, Leste, Oeste };
         static void Main(string[] args)
         {
