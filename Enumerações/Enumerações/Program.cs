b9b504b6449c83a8c91f1970b7dce170d42e7ad4
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-30 10:31:02 -0300

    update Enumerações

---
+++ Enumerações/Enumerações/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Enumerações
+{
+    internal class Program
+    {
+        enum PontoCardeal { Norte, Sul, Leste, Oeste };
+        static void Main(string[] args)
+        {
+            PontoCardeal direcao = PontoCardeal.Norte;            
+            Console.WriteLine("Ponto cardeal: " + direcao );
+        }
+    }
+}
