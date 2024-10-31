c06d9a661dfb8dc5505000f9061293cec31d14bf
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-31 15:25:37 -0300

    update Estruturas_Structures

--- Estruturas_Structures/Estruturas_Structures/Program.cs
+++ Estruturas_Structures/Estruturas_Structures/Program.cs
@@ -6,15 +6,51 @@ namespace Estruturas_Structures
     {
         static void Main(string[] args)
         {
+            /* PRIMEIRO EXEMPLO
             Horario agora; 
             agora.hora = 10;
             agora.minuto = 30;
             Console.WriteLine("Horário: {0}:{1}", agora.hora, agora.minuto);
+            */
+
+            // SEGUNDO EXEMPLO
+            Horario agora = new Horario(11, 25, 00);
+            Console.WriteLine("Agora são: {0} horas", agora.Hora());
+
+            //______________________________________________________________________________________________________________
+
+            // Atribuindo uma nova instância da estrutura 'Horario' com os mesmos valores da instância 'agora'
+            // Isso copia os valores de hora, minuto e segundo para a nova variável 'copia'
+            Horario copia = agora;
+            Console.WriteLine("Agora são: {0} horas", copia.Hora());
         }
 
+        /* PRIMEIRO EXEMPLO
         struct Horario
         {
             public int hora, minuto, segundo;
         }
+        */
+
+        //____________________________________________________________________
+
+        // SEGUNDO EXEMPLO
+        struct Horario
+        {
+            private int hora, minuto, segundo;
+            public Horario(int h, int m, int s)
+            {
+                this.hora = h % 24;
+                this.minuto = m % 60;
+                this.segundo = s % 60;
+            }
+
+            public int Hora()
+            {
+                return this.hora;                
+            }
+            
+        }
+
     }
 }
