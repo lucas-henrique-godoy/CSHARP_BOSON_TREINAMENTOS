e8ebfc01eac20579e49c9e1952e6d03ec13cc83c
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-31 10:22:28 -0300

    criei um novo Console App chamado Estruturas_Structures para estudar sobre Estruturas ou Structs

---
+++ Estruturas_Structures/Estruturas_Structures/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Estruturas_Structures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Horario agora; 
+            agora.hora = 10;
+            agora.minuto = 30;
+            Console.WriteLine("Horário: {0}:{1}", agora.hora, agora.minuto);
+        }
+
+        struct Horario
+        {
+            public int hora, minuto, segundo;
+        }
+
+    }
+}
