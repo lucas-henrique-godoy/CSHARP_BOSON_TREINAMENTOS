0107a47145f031703c3a679add7592b0bf382c2b
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-10-31 17:06:16 -0300

    criei o arquivo Herança_01 para estudar heranças entre classes

---
+++ Herança_01/Herança_01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Channels;
+
+namespace Herança_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Mamifero
+    {
+        public void Respirar()
+        {
+            Console.WriteLine("Eu respiro!");
+        }
+        public void Mamar()
+        {
+            Console.WriteLine("Eu mamo!");
+        }
+    }
+
+    class Humano : Mamifero
+    {
+        public void Falar()
+        {
+            Console.WriteLine("Eu falo!");
+        }
+    }
+
+    class Baleia : Mamifero
+    {
+        public void Nadar()
+        {
+            Console.WriteLine("Eu nado!");
+        }
+    }
+
+    class Gato : Mamifero
+    {
+        public void Arranhar()
+        {
+            Console.WriteLine("Eu arranho!");
+        }
+    }    
+}
