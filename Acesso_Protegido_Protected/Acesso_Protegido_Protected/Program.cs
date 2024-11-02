046984146f0f9b52a1e59fdb328a2afd2d80b779
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-02 19:26:54 -0300

    criei o arquivo  Acesso_Protegido_Protected, para estudar o modificador de acesso protected em OOP.

---
+++ Acesso_Protegido_Protected/Acesso_Protegido_Protected/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Acesso_Protegido_Protected
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
