9246aaf02dc20a9de5f12026faad8eff3502b04e
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-05 10:02:33 -0300

    Criei outro projeto Windows Forms App para estudar como abrir uma janela a partir de outra

---
+++ Janelas2/Janelas2/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Janelas2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAbrir = new System.Windows.Forms.Button();
+            SuspendLayout();
+            // 
+            // btnAbrir
+            // 
+            btnAbrir.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            btnAbrir.Location = new System.Drawing.Point(301, 144);
+            btnAbrir.Name = "btnAbrir";
+            btnAbrir.Size = new System.Drawing.Size(187, 106);
+            btnAbrir.TabIndex = 0;
+            btnAbrir.Text = "Abrir Janela";
+            btnAbrir.UseVisualStyleBackColor = true;
+            btnAbrir.Click += btnAbrir_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(btnAbrir);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAbrir;
+    }
+}
