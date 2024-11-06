e52437f07734f683c97a67181741d610ad0e2cfc
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-06 16:10:42 -0300

    update ANOTACOES

---
+++ Listas_01/Listas_01/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Listas_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> valores = new List<int>();
+            valores.Add(16);
+
+            Console.ReadLine();
+        }
+    }
+}
