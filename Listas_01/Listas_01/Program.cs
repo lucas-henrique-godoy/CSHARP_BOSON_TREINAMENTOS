43d35913c010d85ded531bd7dac56b11580b03a2
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-06 16:14:38 -0300

    Update Program.cs

--- Listas_01/Listas_01/Program.cs
+++ Listas_01/Listas_01/Program.cs
@@ -10,6 +10,14 @@ namespace Listas_01
 
             List<int> valores = new List<int>();
             valores.Add(16);
+            valores.Add(18);
+            valores.Add(23);
+            valores.Add(12);
+
+            int[] numeros = new int[] { 32, 12, 7, 20 };
+            valores.AddRange(numeros);
+
+            Console.WriteLine("A lista possui " + valores.Count() + "elementos ");
 
             Console.ReadLine();
         }
