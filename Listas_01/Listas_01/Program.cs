67fe025b6b19a74565a1ea1551a5337ed8811c49
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-07 09:19:04 -0300

    Update Program.cs

--- Listas_01/Listas_01/Program.cs
+++ Listas_01/Listas_01/Program.cs
@@ -9,7 +9,8 @@ namespace Listas_01
         {
 
             List<int> valores = new List<int>();
-            valores.Add(16);
+
+            valores.Add(16);// Add insere valores ao final da lista
             valores.Add(18);
             valores.Add(23);
             valores.Add(12);
@@ -17,8 +18,15 @@ namespace Listas_01
             int[] numeros = new int[] { 32, 12, 7, 20 };
             valores.AddRange(numeros);// Adiconando outros elementos dentro da lista valores.
 
-            Console.WriteLine("A lista possui " + valores.Count + " elementos ");            
+            //Console.WriteLine("A lista possui " + valores.Count + " elementos ");
+            //Console.WriteLine("O primeiro item é " + valores[0]);
 
+            
+            
+            foreach (int item in valores)
+            {
+                Console.WriteLine(item.ToString());
+            }
             Console.ReadLine();
         }
     }
