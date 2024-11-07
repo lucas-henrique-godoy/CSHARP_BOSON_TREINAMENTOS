f43fcb88d4f636b9ad3d6e8b7e364a6fa98dd98c
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-07 12:50:51 -0300

    Update Form1.cs

--- Trabalhar com ListBox/Trabalhar com ListBox/Form1.cs
+++ Trabalhar com ListBox/Trabalhar com ListBox/Form1.cs
@@ -19,11 +19,11 @@ namespace Trabalhar_com_ListBox
 
         private void btnAdicionaItens_Click(object sender, EventArgs e)
         {
-            lstAnimais.Items.Add("Águia"); // Adiciona o item "Águia" à lista lstAnimais
+            /*lstAnimais.Items.Add("Águia"); // Adiciona o item "Águia" à lista lstAnimais
             lstAnimais.Items.Add("Baleia"); // Adiciona o item "Baleia" à lista lstAnimais
             lstAnimais.Items.Add("Tamanduá"); // Adiciona o item "Tamanduá" à lista lstAnimais
             lstAnimais.Items.Add("Carpa"); // Adiciona o item "Carpa" à lista lstAnimais
-            lstAnimais.Items.Add(txtItem.Text); // Adiciona o conteúdo do controle txtItem (texto digitado pelo usuário) à lista lstAnimais
+            lstAnimais.Items.Add(txtItem.Text); // Adiciona o conteúdo do controle txtItem (texto digitado pelo usuário) à lista lstAnimais*/
 
             // Adiciona todos os itens de um array à lista de uma vez
             string[] animais = new string[7] { "Gato", "Cão", "Papagaio", "Lontra", "Golfinho", "Foca", "Tatu" };
