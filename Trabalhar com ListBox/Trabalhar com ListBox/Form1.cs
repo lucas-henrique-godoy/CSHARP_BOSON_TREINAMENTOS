bcddabea13ca18591a2085b5d57c429be8a68acc
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-07 12:43:49 -0300

    Update Form1.cs

--- Trabalhar com ListBox/Trabalhar com ListBox/Form1.cs
+++ Trabalhar com ListBox/Trabalhar com ListBox/Form1.cs
@@ -20,6 +20,12 @@ namespace Trabalhar_com_ListBox
         private void btnAdicionaItens_Click(object sender, EventArgs e)
         {
             lstAnimais.Items.Add("Águia"); // Adiciona o item "Águia" à lista lstAnimais
+            lstAnimais.Items.Add("Baleia"); // Adiciona o item "Baleia" à lista lstAnimais
+            lstAnimais.Items.Add("Tamanduá"); // Adiciona o item "Tamanduá" à lista lstAnimais
+            lstAnimais.Items.Add("Carpa"); // Adiciona o item "Carpa" à lista lstAnimais
+            lstAnimais.Items.Add(txtItem.Text); // Adiciona o conteúdo do controle txtItem (texto digitado pelo usuário) à lista lstAnimais
+
+
         }
     }
 }
