a35f94cdf5aa2326a84f08d99e5b75e9250e6d07
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-07 15:44:56 -0300

    Programei os botões que removem itens específicos selecionados, 1 item de cada vez e também vários itens selecionados de uma vez.

--- Trabalhar com ListBox/Trabalhar com ListBox/Form1.Designer.cs
+++ Trabalhar com ListBox/Trabalhar com ListBox/Form1.Designer.cs
@@ -32,12 +32,13 @@
             txtItem = new System.Windows.Forms.TextBox();
             lstAnimais = new System.Windows.Forms.ListBox();
             btnMaisItem = new System.Windows.Forms.Button();
+            btnRemoveItens = new System.Windows.Forms.Button();
             SuspendLayout();
             // 
             // btnAdicionaItens
             // 
             btnAdicionaItens.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            btnAdicionaItens.Location = new System.Drawing.Point(39, 212);
+            btnAdicionaItens.Location = new System.Drawing.Point(39, 151);
             btnAdicionaItens.Name = "btnAdicionaItens";
             btnAdicionaItens.Size = new System.Drawing.Size(176, 64);
             btnAdicionaItens.TabIndex = 0;
@@ -50,7 +51,7 @@
             txtItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             txtItem.Location = new System.Drawing.Point(39, 82);
             txtItem.Name = "txtItem";
-            txtItem.Size = new System.Drawing.Size(122, 29);
+            txtItem.Size = new System.Drawing.Size(176, 29);
             txtItem.TabIndex = 1;
             // 
             // lstAnimais
@@ -60,13 +61,14 @@
             lstAnimais.ItemHeight = 24;
             lstAnimais.Location = new System.Drawing.Point(327, 82);
             lstAnimais.Name = "lstAnimais";
+            lstAnimais.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
             lstAnimais.Size = new System.Drawing.Size(120, 244);
             lstAnimais.TabIndex = 2;
             // 
             // btnMaisItem
             // 
             btnMaisItem.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            btnMaisItem.Location = new System.Drawing.Point(39, 308);
+            btnMaisItem.Location = new System.Drawing.Point(39, 251);
             btnMaisItem.Name = "btnMaisItem";
             btnMaisItem.Size = new System.Drawing.Size(176, 64);
             btnMaisItem.TabIndex = 3;
@@ -74,17 +76,30 @@
             btnMaisItem.UseVisualStyleBackColor = true;
             btnMaisItem.Click += btnMaisItem_Click;
             // 
+            // btnRemoveItens
+            // 
+            btnRemoveItens.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            btnRemoveItens.Location = new System.Drawing.Point(39, 358);
+            btnRemoveItens.Name = "btnRemoveItens";
+            btnRemoveItens.Size = new System.Drawing.Size(176, 64);
+            btnRemoveItens.TabIndex = 4;
+            btnRemoveItens.Text = "Remover Itens";
+            btnRemoveItens.UseVisualStyleBackColor = true;
+            btnRemoveItens.Click += btnRemoveItens_Click;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             ClientSize = new System.Drawing.Size(482, 490);
+            Controls.Add(btnRemoveItens);
             Controls.Add(btnMaisItem);
             Controls.Add(lstAnimais);
             Controls.Add(txtItem);
             Controls.Add(btnAdicionaItens);
             Name = "Form1";
             Text = "Form1";
+            Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
         }
@@ -95,5 +110,6 @@
         private System.Windows.Forms.TextBox txtItem;
         private System.Windows.Forms.ListBox lstAnimais;
         private System.Windows.Forms.Button btnMaisItem;
+        private System.Windows.Forms.Button btnRemoveItens;
     }
 }
--- Trabalhar com ListBox/Trabalhar com ListBox/Form1.cs
+++ Trabalhar com ListBox/Trabalhar com ListBox/Form1.cs
@@ -1,48 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System; // Importa o namespace System, que contém classes básicas para trabalhar com tipos básicos, como String, Int, etc.
+using System.Collections.Generic; // Importa o namespace para coleções genéricas, como listas, dicionários, etc.
+using System.ComponentModel; // Importa o namespace para componentes e eventos relacionados ao design do formulário.
+using System.Data; // Importa o namespace que contém classes para trabalhar com dados, como DataTables, DataSets, etc.
+using System.Drawing; // Importa o namespace para trabalhar com gráficos, imagens e desenhos.
+using System.Linq; // Importa o namespace que contém LINQ, uma forma de consulta para coleções.
+using System.Text; // Importa o namespace para trabalhar com codificação e manipulação de strings.
+using System.Threading.Tasks; // Importa o namespace para trabalhar com tarefas assíncronas.
+using System.Windows.Forms; // Importa o namespace que contém classes para criar aplicações com formulários e controles gráficos.
 
-namespace Trabalhar_com_ListBox
+namespace Trabalhar_com_ListBox // Define o namespace do projeto, onde o código está contido.
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form // Define a classe Form1, que herda de 'Form', representando o formulário (janela gráfica).
     {
-        public Form1()
+        public Form1() // Construtor da classe Form1
         {
-            InitializeComponent();
+            InitializeComponent(); // Inicializa os componentes do formulário, como botões, caixas de texto, etc.
         }
 
+        // Método que é chamado quando o botão 'btnAdicionaItens' é clicado.
         private void btnAdicionaItens_Click(object sender, EventArgs e)
         {
+            // Comentado, pois foi substituído por outras implementações. Aqui, os itens são adicionados manualmente.
             /*lstAnimais.Items.Add("Águia"); // Adiciona o item "Águia" à lista lstAnimais
             lstAnimais.Items.Add("Baleia"); // Adiciona o item "Baleia" à lista lstAnimais
             lstAnimais.Items.Add("Tamanduá"); // Adiciona o item "Tamanduá" à lista lstAnimais
             lstAnimais.Items.Add("Carpa"); // Adiciona o item "Carpa" à lista lstAnimais
             lstAnimais.Items.Add(txtItem.Text); // Adiciona o conteúdo do controle txtItem (texto digitado pelo usuário) à lista lstAnimais*/
+        }
 
-            // Adiciona os itens de um array à lista, se a lista estiver vazia
-            string[] animais = new string[7] { "Gato", "Cão", "Papagaio", "Lontra", "Golfinho", "Foca", "Tatu" }; // Cria um array com 7 nomes de animais
+        // Método que é chamado quando o botão 'btnMaisItem' é clicado.
+        private void btnMaisItem_Click(object sender, EventArgs e)
+        {
+            // Insere o texto digitado no TextBox (txtItem) no início da lista 'lstAnimais'.
+            lstAnimais.Items.Insert(0, txtItem.Text); // Insere o texto digitado em txtItem no início da lista lstAnimais
+        }
 
-            if (lstAnimais.Items.Count == 0) // Verifica se a lista está vazia (não possui itens)
+        // Método que é chamado quando o botão 'btnRemoveItens' é clicado.
+        private void btnRemoveItens_Click(object sender, EventArgs e)
+        {
+            /*if (lstAnimais.SelectedIndex != -1) // Verifica se um item foi selecionado na lista
             {
-                lstAnimais.Items.AddRange(animais); // Adiciona todos os itens do array 'animais' à lista lstAnimais
+                lstAnimais.Items.RemoveAt(lstAnimais.SelectedIndex); // Remove o item selecionado da lista lstAnimais
             }
             else
             {
-                MessageBox.Show("Lista já preenchida!"); // Exibe uma mensagem caso a lista já tenha itens
+                MessageBox.Show("Selecione um elemento!"); // Exibe uma mensagem de erro se nenhum item estiver selecionado
+            }*/
+
+            /*lstAnimais.Items.Remove(lstAnimais.SelectedItem); // Remove o item selecionado diretamente
+            lstAnimais.Items.Remove("Gato"); // Remove especificamente o item "Gato" da lista*/
+
+            // Loop que percorre os itens selecionados da lista e os remove.
+            // Fazemos isso de trás para frente para evitar problemas com a alteração do índice à medida que os itens são removidos.
+            for (int i = lstAnimais.SelectedIndices.Count - 1; i >= 0; i--) // Itera pelos índices selecionados da lista, do último para o primeiro.
+            {
+                lstAnimais.Items.RemoveAt(lstAnimais.SelectedIndices[i]); // Remove o item na posição selecionada na lista
             }
-
-
         }
 
-        private void btnMaisItem_Click(object sender, EventArgs e)
+        // Método que é chamado quando o formulário é carregado (ao iniciar o programa).
+        private void Form1_Load(object sender, EventArgs e)
         {
-            lstAnimais.Items.Insert(0, txtItem.Text); // Insere o texto digitado em txtItem no início da lista lstAnimais
+            // Adiciona os itens de um array à lista, mas somente se a lista estiver vazia.
+            string[] animais = new string[7] { "Gato", "Cão", "Papagaio", "Lontra", "Golfinho", "Foca", "Tatu" }; // Cria um array de strings com 7 nomes de animais
+
+            lstAnimais.Items.AddRange(animais); // Adiciona todos os itens do array 'animais' à lista lstAnimais de uma vez
         }
     }
 }
