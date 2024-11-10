75e913ed743718911f80f6499b0171d9caa3a9b4
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-09 23:34:25 -0300

    Update Program.cs

--- Laço For 2/Laço For 2/Program.cs
+++ Laço For 2/Laço For 2/Program.cs
@@ -6,10 +6,17 @@ namespace Laço_For_2
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <= 50; i++)
+            // Declara a variável inteira 'i' que será usada como contador no laço.
+            int i;
+
+            // Inicia um laço 'for' que vai se repetir enquanto 'i' for menor ou igual a 150.
+            for (i = 0; i <= 150; i++) // 1ª parte: inicializa 'i' com 0, 2ª parte: a condição para o loop continuar, 3ª parte: incrementa 'i' a cada iteração.
             {
-                Console.WriteLine(i.ToString());
+                // A cada iteração do laço, calcula o valor de '500 + i * 2' e exibe esse valor no console.
+                Console.WriteLine(500 + i * 2);
+                // '500 + i * 2' gera uma sequência de números, começando em 500 e aumentando de 2 em 2.
             }
+
         }
     }
 }
