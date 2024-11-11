529244004bae0a76362f80d5912176860ecfbb79
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-11 10:46:44 -0300

    Update Program.cs

--- Break/Break/Program.cs
+++ Break/Break/Program.cs
@@ -6,21 +6,25 @@ namespace Break
     {
         static void Main(string[] args)
         {
-            int numero;
-            while (true)
+            int numero;  // Declara uma variável inteira chamada 'numero'
+
+            while (true)  // Inicia um laço infinito, que continuará até ser interrompido por um comando 'break'
             {
-                Console.WriteLine("Entre com um número; 15 para sair: ");
-                numero = int.Parse(Console.ReadLine());
-                if (numero == 15)
+                Console.WriteLine("Entre com um número; 15 para sair: ");  // Exibe uma mensagem pedindo para o usuário digitar um número
+                numero = int.Parse(Console.ReadLine());  // Lê a entrada do usuário, converte de string para inteiro e armazena na variável 'numero'
+
+                if (numero == 15)  // Verifica se o número digitado é 15
                 {
-                    break;
+                    break;  // Se o número for 15, sai do laço 'while' com o comando 'break'
                 }
-                else
+                else  // Se o número não for 15
                 {
-                    Console.WriteLine("Número digitado foi: {0}" , numero);                    
+                    Console.WriteLine("Número digitado foi: {0}", numero);  // Exibe o número que o usuário digitou
                 }
             }
-            Console.WriteLine("Laço while foi encerrado! ");
+
+            Console.WriteLine("Laço while foi encerrado! ");  // Após sair do laço, exibe uma mensagem indicando que o laço foi encerrado
+
         }
     }
 }
