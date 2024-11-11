ff25338d6b16b53a4f6e263b5d82bee2f6215bc7
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-11 10:43:25 -0300

    Criei o Continue para estudar a declaração continue em C#

---
+++ Break/Break/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Break
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            while (true)
+            {
+                Console.WriteLine("Entre com um número; 15 para sair: ");
+                numero = int.Parse(Console.ReadLine());
+                if (numero == 15)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Número digitado foi: {0}" , numero);                    
+                }
+            }
+            Console.WriteLine("Laço while foi encerrado! ");
+        }
+    }
+}
---
+++ Continue/Continue/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Continue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int numero;  // Declara uma variável 'numero' do tipo inteiro
+
+            while (true)  // Inicia um laço infinito que continuará até ser explicitamente interrompido
+            {
+                Console.WriteLine("Digite um número entre 10 e 20: ");  // Exibe uma mensagem pedindo ao usuário que digite um número
+                numero = int.Parse(Console.ReadLine());  // Lê a entrada do usuário, converte para inteiro e armazena na variável 'numero'
+
+                if ((numero < 10) || (numero > 20))  // Verifica se o número digitado está fora do intervalo 10-20
+                {
+                    continue;  // Se o número estiver fora do intervalo, o laço volta ao início, pedindo novamente a entrada
+                }
+                else  // Se o número estiver dentro do intervalo 10-20
+                {
+                    Console.WriteLine("Número digitado: {0}", numero);  // Exibe o número que foi digitado
+                }
+
+                Console.WriteLine("Vamos ao próximo valor!");  // Exibe uma mensagem indicando que o próximo valor será solicitado
+            }
+
+
+        }
+    }
+}
