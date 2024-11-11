21e91d5714edba2b24add0e149dd75ae3eff41e7
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-10 22:48:01 -0300

    Criei o Laço  While 2 para treinar laços de repetição

---
+++ Laço Wile 2/Laço Wile 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Laço_Wile_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            while (contador <= 10)
+            {
+                Console.WriteLine(contador.ToString()); 
+                contador++;
+            }
+
+            int contador2 = 10;
+            while (contador2 >= 0)
+            {
+                Console.WriteLine(contador2.ToString());
+                contador2--;
+            }
+        }
+    }
+}
