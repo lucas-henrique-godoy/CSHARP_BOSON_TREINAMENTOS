d2c164761291b9ec6d76f59c03c99a7fbffaeea6
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2024-11-10 23:15:41 -0300

    Update Program.cs

--- Laço Wile 2/Laço Wile 2/Program.cs
+++ Laço Wile 2/Laço Wile 2/Program.cs
@@ -6,12 +6,33 @@ namespace Laço_Wile_2
     {
         static void Main(string[] args)
         {
+            /*
             int contador = 1;
             while (contador <= 50)
             {
                 Console.WriteLine(contador.ToString()); 
                 contador++;
-            }            
+            } 
+            */
+
+            Console.WriteLine("Digite um número: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if ((num == 0) || (num < 0))
+            {
+                Console.WriteLine("Erro, número negativo ou igual a zero");
+            }
+            else
+            {
+                int i = 1;
+                while (i <= num)
+                {
+                    Console.WriteLine(i.ToString());
+                    i++;
+                }
+            }
+
+            
         }
     }
 }
